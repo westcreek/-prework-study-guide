0f2cf750915b92977013a8c9ac095036e6de0b25
Meghan Doherty <parkoperationsmanager@Meghans-MacBook-Pro.local>
2023-01-30 14:36:17 -0600

    Update project README file

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,6 @@ if (randomTopic === 'HTML') {
 
 console.log('Here are the topics we learned through Prework:');
 listTopics()
-
 console.log('Which topic should we study first?');
 selectTopic()
 
